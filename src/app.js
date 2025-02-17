5b9d031eeb45ab06b98bc07d81e65cdd7744db05
Felipe Moreno <85708817+blacklottus-09@users.noreply.github.com>
2025-02-17 04:38:27 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 import "bootstrap";
 import "./style.css";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function excuseGenerator(who, action, what, when) {
+    let numWho = Math.floor(Math.random() * who.length);
+    let numAction = Math.floor(Math.random() * action.length);
+    let numWhat = Math.floor(Math.random() * what.length);
+    let numWhen = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    return `${who[numWho]} ${action[numAction]} ${what[numWhat]} ${when[numWhen]}.`;
+}
+
+function onLoad() {
+    let excuseElement = document.getElementById('excuse');
+    if (excuseElement) {
+        excuseElement.innerHTML = excuseGenerator(who, action, what, when);
+    } else {
+        console.error("Element with ID 'excuse' not found.");
+    }
+}
+
+window.onload = onLoad;
+
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.getElementById("generateExcuse");
+    if (button) {
+        button.addEventListener("click", function () {
+            document.getElementById("excuse").innerHTML = excuseGenerator(who, action, what, when);
+        });
+    }
+});
